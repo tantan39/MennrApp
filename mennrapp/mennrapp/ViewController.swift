9fbf4425b2e4a223b3300ff4b3136871c322f7c5
Tan Tran <tantt.dev@gmail.com>
2022-05-27 14:34:32 -0400

    Init project

---
+++ mennrapp/mennrapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mennrapp
+//
+//  Created by Tan Tan on 5/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
