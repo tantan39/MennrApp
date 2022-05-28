05506e61b5ce27f61247e82795c5d636ac5d6bd9
Tan Tran <tantt.dev@gmail.com>
2022-05-28 09:59:31 -0400

    Add `DetailsViewController`

---
+++ mennrapp/mennrapp/DetailsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailsViewController.swift
+//  mennrapp
+//
+//  Created by Tan Tan on 5/28/22.
+//
+
+import Foundation
+
+class DetailsViewController: ViewController {
+    override func viewDidLoad() {
+
+    }
+}
